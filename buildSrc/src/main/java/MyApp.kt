bead70079eb0ac02858107725c6a61004d67a6fd
Butanediol <n872485238@gmail.com>
2022-03-24 20:02:53 +0800

    整理年久失修的链接、图片、说明

--- buildSrc/src/main/java/MyApp.kt
+++ buildSrc/src/main/java/MyApp.kt
@@ -2,6 +2,6 @@
 
 object MyApp {
     const val appId = "com.oblatum.iconpack"
-    const val version = 269
-    const val versionName = "2.20b"
+    const val version = 270
+    const val versionName = "2.20c"
 }
