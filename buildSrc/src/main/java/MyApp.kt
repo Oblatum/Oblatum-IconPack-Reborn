40b19f4528c5970a50c229ccb5c984e7b6c1b884
PzHown <1263097055@qq.com>
2024-07-31 18:01:31 +0800

    chore: 更新版本号至3.0.24.7.31

--- buildSrc/src/main/java/MyApp.kt
+++ buildSrc/src/main/java/MyApp.kt
@@ -2,6 +2,6 @@
 
 object MyApp {
     const val appId = "com.oblatum.iconpack"
-    const val version = 275
-    const val versionName = "3.0.24.6.6_FIX"
+    const val version = 276
+    const val versionName = "3.0.24.7.31"
 }
