48cfa9fc59940eff97085233bec1bb16ab4316d7
PzHown <1263097055@qq.com>
2025-07-07 11:49:36 +0800

    update

--- buildSrc/src/main/java/MyApp.kt
+++ buildSrc/src/main/java/MyApp.kt
@@ -2,6 +2,6 @@
 
 object MyApp {
     const val appId = "com.oblatum.iconpack"
-    const val version = 279
-    const val versionName = "3.0.25.4.1"
+    const val version = 280
+    const val versionName = "3.0.25.7.7"
 }
