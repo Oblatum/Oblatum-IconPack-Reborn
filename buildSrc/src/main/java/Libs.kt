3bea770c4ab489db6eede3824cbbf8397f6ab890
Butanediol <love@xnh.app>
2025-12-18 00:59:27 +0800

    libs: 2.5.0.1-icon-retrieval-fix

--- buildSrc/src/main/java/Libs.kt
+++ buildSrc/src/main/java/Libs.kt
@@ -5,7 +5,7 @@ object Libs {
     const val kotlin = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlin}"
 
     // Blueprint
-    const val blueprint = "com.github.Oblatum:Blueprint:${Versions.blueprint}-apptracker"
+    const val blueprint = "com.github.Oblatum:Blueprint:${Versions.blueprint}-icon-retrieval-fix"
 
     // OneSignal
     const val oneSignal = "com.onesignal:OneSignal:${Versions.oneSignal}"
